7392bbcac635c07ecf7e95591b8b0c0ed3be6398
E <owl.robot@aol.com>
2019-03-21 16:00:20 -0400

    Update listcompexample.py

--- listcompexample.py
+++ listcompexample.py
@@ -1,3 +1,5 @@
+# Listcomp example.
+
 colors = ['black', 'white']
 sizes = ['S', 'M', 'L']
 tshirts = [(color, size) for color in colors for size in sizes]
@@ -7,4 +9,12 @@ print(tshirts)
 for color in colors:
     for size in sizes:
         print((color, size))
+        
+        
+# Genexp example. 
+
+colors = ['black', 'white']
+sizes = ['S', 'M', 'L']
+for tshirt in ('%s %s' % (c, s) for c in colors for s in sizes):
+    print(tshirt)    
         
