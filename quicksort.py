826aa69cc0c1f488ae5d582e908603a6f50361c4
Edie White <owl.robot@aol.com>
2019-04-05 16:10:02 -0400

    Add files via upload

---
+++ quicksort.py
@@ -0,0 +1,42 @@
+def quickSort(mylist):
+   quickSortHelper(mylist,0,len(mylist)-1)
+
+def quickSortHelper(mylist,first,last):
+   if first<last:
+
+       splitpoint = partition(mylist,first,last)
+
+       quickSortHelper(mylist,first,splitpoint-1)
+       quickSortHelper(mylist,splitpoint+1,last)
+
+def partition(mylist,first,last):
+   pivotvalue = mylist[first]
+
+   leftmark = first+1
+   rightmark = last
+
+   done = False
+   while not done:
+
+       while leftmark <= rightmark and mylist[leftmark] <= pivotvalue:
+           leftmark = leftmark + 1
+
+       while mylist[rightmark] >= pivotvalue and rightmark >= leftmark:
+           rightmark = rightmark -1
+
+       if rightmark < leftmark:
+           done = True
+       else:
+           temp = mylist[leftmark]
+           mylist[leftmark] = mylist[rightmark]
+           mylist[rightmark] = temp
+
+   temp = mylist[first]
+   mylist[first] = mylist[rightmark]
+   mylist[rightmark] = temp
+
+   return rightmark
+
+mylist = [54,26,93,17,77,31,44,55,20]
+quickSort(mylist)
+print(mylist)
