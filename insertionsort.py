02150007248abdec2d4b6e679c5d4c58474965ac
Edie White <owl.robot@aol.com>
2019-04-09 16:11:42 -0400

    Update insertionsort.py

--- insertionsort.py
+++ insertionsort.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 
-
 def insertion_sort(collection):
     for index in range(1, len(collection)):
         while index > 0 and collection[index - 1] > collection[index]:
