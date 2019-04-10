76c164311bc90428c70b2b9ae6f96cdbcc2f8344
Edie White <owl.robot@aol.com>
2019-04-10 17:08:01 -0400

    Add files via upload

---
+++ mergesort.py
@@ -0,0 +1,39 @@
+# Python3.7.3
+
+def mergeSort(alist):
+    print("Splitting ", alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ", alist)
+
+alist = [14, 22, 33, 43, 15, 27, 88, 86, 100]
+mergeSort(alist)
+print(alist)
+
