18bc7859fad26b5a8a8a4d780ad8958acdd1dbf3
Edie W <owl.robot@aol.com>
2019-04-12 12:09:26 -0400

    Update linearsearch.py

--- linearsearch.py
+++ linearsearch.py
@@ -1,17 +1,16 @@
 # Python3.7.3
 
-def search(arr, n, x): 
+def search(arr, len, x): 
   
-    for i in range (0, n): 
+    for i in range (0, len): 
         if (arr[i] == x): 
             return i; 
     return -1; 
   
-# Driver Code 
-arr = [ 2, 3, 4, 10, 40 ]; 
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
 x = 10; 
-n = len(arr); 
-result = search(arr, n, x) 
+len = len(arr); 
+result = search(arr, len, x) 
 if(result == -1): 
     print("Element is not in this array.") 
 else: 
