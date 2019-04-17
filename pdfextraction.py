d7680a9ec5abc1c056508ebfae8bf56cbe59240c
Edie W <owl.robot@aol.com>
2019-04-17 19:33:57 -0400

    Update pdfextraction.py

--- pdfextraction.py
+++ pdfextraction.py
@@ -7,8 +7,6 @@ pdfFileObj = open('meetingminutes.pdf', 'rb')
   
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
   
-print(pdfReader.numPages) 
-  
 pageObj = pdfReader.getPage(0) 
   
 print(pageObj.extractText())
