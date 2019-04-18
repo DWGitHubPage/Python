c46ee1e6974cb551fcef3d4cba595429f9a9a658
Edie W <owl.robot@aol.com>
2019-04-18 11:39:46 -0400

    Add files via upload

---
+++ descriptorExample.py
@@ -0,0 +1,36 @@
+class Quantity:
+    __counter = 0
+
+    def __init__(self):
+        cls = self.__class__
+        prefix = cls.__name__
+        index = cls.__counter
+        self.storage_name = '_{}#{}'.format(prefix, index)
+        cls.__counter += 1
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        else:
+            return getattr(instance, self.storage_name)
+
+    def __set__(self, instance, value):
+        if value > 0:
+            setattr(instance, self.storage_name, value)
+        else:
+            raise ValueError('value must be > 0')
+
+class LineItem:
+    weight = Quantity()
+    price = Quantity()
+
+    def __init__(self, description, weight, price):
+        self.description = description
+        self.weight = weight
+        self.price = price
+
+    def subtotal(self):
+        return self.weight * self.price
+
+br_nuts = LineItem('Brazil nuts', 10, 34.95)
+print(br_nuts.price)
