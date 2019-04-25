7dbf6ffa9a2f45b8586ecff8aa4b019c6f7c4cf9
Edie W <owl.robot@aol.com>
2019-04-25 16:02:48 -0400

    Add files via upload

---
+++ euclidianaglorithm(gcd).py
@@ -0,0 +1,21 @@
+# Python3.7.3
+
+from math import *
+
+""" (GCD) Greatest Common Denominator also called (HCF)
+Highest Common Factor."""
+
+def gcd(x, y, verbose=True):
+	if x < y: 
+		return gcd(y, x, verbose)
+	print()
+	while y != 0:
+		if verbose: print('%s = %s * %s + %s' % (x, floor(x/y), y, x % y))
+		(x, y) = (y, x % y)
+	
+	if verbose: print('gcd is %s' % x) 
+	return x
+
+gcd(400, 333)
+gcd(236, 8)
+gcd(12, 300)
