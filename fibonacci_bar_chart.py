3db46d3188709605dd6a1299ba8677d4cf496f16
Edie W <owl.robot@aol.com>
2019-04-29 15:35:46 -0400

    Add files via upload

---
+++ fibonacci_bar_chart.py
@@ -0,0 +1,7 @@
+# Python3.7.3
+# Fibonacci bar chart.
+
+import pygal                                                       
+bar_chart = pygal.Bar()                                            
+bar_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])  
+bar_chart.render_to_file('bar_chart.svg')   
