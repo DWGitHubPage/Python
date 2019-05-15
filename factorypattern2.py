78fd0b919da53aed314a4c37dfbb4810db61153c
Edie W <owl.robot@aol.com>
2019-05-15 13:47:38 -0400

    Add files via upload

---
+++ factorypattern2.py
@@ -0,0 +1,21 @@
+class Car(object):
+
+    def factory(type):
+        if type == "Racecar":
+            return Racecar()
+        if type == "Van":
+            return Van()
+
+    factory = staticmethod(factory)
+
+class Racecar(Car):
+    def drive(self):
+        print("Racecar driving.")
+
+class Van(Car):
+    def drive(self):
+        print("Van driving.")
+
+# Create object using factory.
+obj = Car.factory("Racecar")
+obj.drive()
