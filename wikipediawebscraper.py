df5e7b57573af3a5c21a0a47c138680962aeaafe
Edie W <owl.robot@aol.com>
2019-06-06 14:17:53 -0400

    Add files via upload

---
+++ wikipediawebscraper.py
@@ -0,0 +1,19 @@
+# Python3.7.3
+# Wikipedia API scraper.
+
+import wikipedia
+
+print(wikipedia.search("Peter"))
+print("\n")
+print(wikipedia.search("Peter", results=4))
+print("\n")
+print(wikipedia.suggest("Peter Dinlage"))
+print("\n")
+print(wikipedia.summary("Python"))
+print("\n")
+print(wikipedia.summary("Python", sentences=2))
+print("\n")
+print(wikipedia.summary("Key (cryptography)"))  
+print("\n")
+print(wikipedia.page("Python").content)
+
