3880842d635418df2a9534c3f158a5beb82e60c5
Edie W <owl.robot@aol.com>
2019-06-22 13:00:32 -0400

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+# Python3.7.3
+#Fibonacci module to use.
+
+
+def fib(n):    
+    a, b = 0, 1
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a+b
+    print()
+
+"""Then use:
+
+import fibonacci
+print(fibonacci.fib(35))
+
+To run it up to whatever number you want."""
