5611b1bbfc7e4499524f8d7ea8c3a5c4fa791ff5
D. W <owl.robot@aol.com>
2020-03-19 13:54:36 -0400

    Add files via upload

---
+++ startoftheday.py
@@ -0,0 +1,11 @@
+# Python 3.8.2
+# Start of the day script.
+
+import webbrowser
+
+webbrowser.open('https://www.aol.com')
+webbrowser.open('https://news.ycombinator.com')
+webbrowser.open('https://www.itworld.com/news/')
+webbrowser.open('https://dailytechnewsshow.com')
+
+
