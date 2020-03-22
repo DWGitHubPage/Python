9d5236f7cde5c64e1cca008cf205de0038f804d6
D. W <owl.robot@aol.com>
2020-03-22 15:21:53 -0400

    Add files via upload

---
+++ itcrowd.py
@@ -0,0 +1,12 @@
+# Python 3.8.2
+
+import os
+
+a = input ("Are you having trouble with your computer? ")
+
+
+if a == ("Yes") or a == ("yes"):
+    os.system("say 'Have you tried turning it off and on again?'")
+
+elif a == ("No") or a == ("no"):
+  exit
